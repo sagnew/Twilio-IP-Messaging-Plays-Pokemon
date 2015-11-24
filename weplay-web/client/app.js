685784dd74bf777f4e933949ffd693ac7c43b424
Sam Agnew <sagnew@eden.rutgers.edu>
2015-11-24 06:10:56 -0500

    Restructure client side app to be more modular and clean

--- weplay-web/client/app.js
+++ weplay-web/client/app.js
@@ -9,6 +9,24 @@ var socket = io(config.io);
 var joined = false;
 var input = $('.input input');
 var nick;
+var image = $('#game img')[0];
+var lastImage;
+var map = {
+  37: 'left',
+  39: 'right',
+  65: 'a',
+  83: 'b',
+  66: 'b',
+  38: 'up',
+  40: 'down',
+  79: 'select',
+  13: 'start'
+};
+
+var reverseMap = {};
+for (var i in map) reverseMap[map[i]] = i;
+
+// Global functions:
 
 // resize asap before loading other stuff
 function resize() {
@@ -22,12 +40,45 @@ function resize() {
     $('.messages').css('height', $('#chat').height() - 70);
   }
 }
+
+function join(data) {
+  nick = data;
+  // Try-catch necessary because Safari might have locked setItem causing
+  // exception
+  try {
+    if (window.localStorage) localStorage.nick = data;
+  } catch (e) {}
+
+  $('body').addClass('joined');
+  $('.input').addClass('joined');
+  input
+  .attr('placeholder', 'type in to chat')
+  .blur();
+  joined = true;
+
+  // If the IP Messaging channel doesn't exist yet, initialize it.
+  if(!chat.channel) {
+    chat.initializeChat(nick);
+  }
+}
+
+// Highlights controls when image or button pressed
+function highlightControls() {
+  $('table.screen-keys td:not(.empty-cell)').addClass('highlight');
+
+  setTimeout(function() {
+    $('table.screen-keys td').removeClass('highlight');
+  }, 300);
+}
+
+// Resize the window right away
 $(window).resize(resize);
 resize();
 
 // reset game img size for mobile now that we loaded
 $('#game img').css('height', '100%');
 
+// Socket.io event listeners
 socket.on('connect', function() {
   $('body').addClass('ready');
   $('.messages').empty();
@@ -51,9 +102,93 @@ socket.on('disconnect', function() {
   printMessage('Disconnected. Reconnecting.');
 });
 
+socket.on('joined', function() {
+  $('.messages').append(
+    $('<p>').text('You have joined.').append($('<span class="key-info"> Keys are as follows: </span>'))
+    .append(
+    $('<table class="keys">').append(
+      $('<tr><td>left</td><td>←</td>'),
+      $('<tr><td>right</td><td>→</td>'),
+      $('<tr><td>up</td><td>↑</td>'),
+      $('<tr><td>down</td><td>↓</td>'),
+      $('<tr><td>A</td><td>a</td>'),
+      $('<tr><td>B</td><td>s</td>'),
+      $('<tr><td>select</td><td>o</td>'),
+      $('<tr><td>start</td><td>enter</td>')
+    ))
+    .append('<br><span class="key-info">Make sure the chat input is not focused to perform moves.</span><br> '
+      + 'Input is throttled server side to prevent abuse. Catch \'em all!')
+  );
+
+  $('table.unjoined').removeClass('unjoined');
+});
+
+socket.on('connections', function(total) {
+  $('.count').text(total);
+});
+
+socket.on('join', function(nick, loc) {
+  var p = $('<p>');
+  p.append($('<span class="join-by">').text(nick));
+  if (loc) {
+    p.append(' (' + loc + ')');
+  }
+  p.append(' joined.');
+  $('.messages').append(p);
+  chat.trimMessages();
+});
+
+// Uncomment the following listener to allow users to be notified when a button is pressed.
+//socket.on('move', function(move, by) {
+//  var p = $('<p class="move">').text(' pressed ' + move);
+//  p.prepend($('<span class="move-by">').text(by));
+//  $('.messages').append(p);
+//  chat.trimMessages();
+//});
+
+socket.on('reload', function() {
+  setTimeout(function() {
+    location.reload();
+  }, Math.floor(Math.random() * 10000) + 5000);
+});
+
+socket.on('frame', function(data) {
+  if (lastImage && 'undefined' != typeof URL) {
+    URL.revokeObjectURL(lastImage);
+  }
+  image.src = blobToImage(data);
+  lastImage = image.src;
+});
+
 if ('ontouchstart' in window) {
   $('body').addClass('touch');
 }
+
+input.focus(function() {
+  $('body').addClass('input_focus');
+});
+
+input.blur(function() {
+  $('body').removeClass('input_focus');
+});
+
+// Listener to fire up keyboard events on mobile devices for control overlay
+$('table.screen-keys td').mousedown(function() {
+  var id = $(this).attr('id');
+  var code = reverseMap[id];
+  var e = $.Event('keydown');
+  e.keyCode = code;
+  $(document).trigger(e);
+
+  $(this).addClass('pressed');
+  var self = this;
+  setTimeout(function() {
+    $(self).removeClass('pressed');
+  }, 1000);
+});
+
+$('img').mousedown(highlightControls);
+$('table.screen-keys td').mousedown(highlightControls);
 
 $('.input form').submit(function(ev) {
   // First prevent the form from being submitted
@@ -89,134 +224,3 @@ $('.input form').submit(function(ev) {
     join(enteredText);
   }
 });
-
-function join(data) {
-  nick = data;
-  // Try-catch necessary because Safari might have locked setItem causing
-  // exception
-  try {
-    if (window.localStorage) localStorage.nick = data;
-  } catch (e) {}
-
-  $('body').addClass('joined');
-  $('.input').addClass('joined');
-  input
-  .attr('placeholder', 'type in to chat')
-  .blur();
-  joined = true;
-
-  // If the IP Messaging channel doesn't exist yet, initialize it.
-  if(!chat.channel) {
-    chat.initializeChat(nick);
-  }
-}
-
-input.focus(function() {
-  $('body').addClass('input_focus');
-});
-
-input.blur(function() {
-  $('body').removeClass('input_focus');
-});
-
-socket.on('joined', function() {
-  $('.messages').append(
-    $('<p>').text('You have joined.').append($('<span class="key-info"> Keys are as follows: </span>'))
-    .append(
-    $('<table class="keys">').append(
-      $('<tr><td>left</td><td>←</td>'),
-      $('<tr><td>right</td><td>→</td>'),
-      $('<tr><td>up</td><td>↑</td>'),
-      $('<tr><td>down</td><td>↓</td>'),
-      $('<tr><td>A</td><td>a</td>'),
-      $('<tr><td>B</td><td>s</td>'),
-      $('<tr><td>select</td><td>o</td>'),
-      $('<tr><td>start</td><td>enter</td>')
-    ))
-    .append('<br><span class="key-info">Make sure the chat input is not focused to perform moves.</span><br> '
-      + 'Input is throttled server side to prevent abuse. Catch \'em all!')
-  );
-
-  $('table.unjoined').removeClass('unjoined');
-});
-
-var map = {
-  37: 'left',
-  39: 'right',
-  65: 'a',
-  83: 'b',
-  66: 'b',
-  38: 'up',
-  40: 'down',
-  79: 'select',
-  13: 'start'
-};
-
-var reverseMap = {};
-for (var i in map) reverseMap[map[i]] = i;
-
-// Listener to fire up keyboard events on mobile devices for control overlay
-$('table.screen-keys td').mousedown(function() {
-  var id = $(this).attr('id');
-  var code = reverseMap[id];
-  var e = $.Event('keydown');
-  e.keyCode = code;
-  $(document).trigger(e);
-
-  $(this).addClass('pressed');
-  var self = this;
-  setTimeout(function() {
-    $(self).removeClass('pressed');
-  }, 1000);
-});
-
-socket.on('connections', function(total) {
-  $('.count').text(total);
-});
-
-socket.on('join', function(nick, loc) {
-  var p = $('<p>');
-  p.append($('<span class="join-by">').text(nick));
-  if (loc) {
-    p.append(' (' + loc + ')');
-  }
-  p.append(' joined.');
-  $('.messages').append(p);
-  chat.trimMessages();
-});
-
-// Uncomment the following listener to allow users to be notified when a button is pressed.
-//socket.on('move', function(move, by) {
-//  var p = $('<p class="move">').text(' pressed ' + move);
-//  p.prepend($('<span class="move-by">').text(by));
-//  $('.messages').append(p);
-//  chat.trimMessages();
-//});
-
-socket.on('reload', function() {
-  setTimeout(function() {
-    location.reload();
-  }, Math.floor(Math.random() * 10000) + 5000);
-});
-
-var image = $('#game img')[0];
-var lastImage;
-socket.on('frame', function(data) {
-  if (lastImage && 'undefined' != typeof URL) {
-    URL.revokeObjectURL(lastImage);
-  }
-  image.src = blobToImage(data);
-  lastImage = image.src;
-});
-
-// Highlights controls when image or button pressed
-function highlightControls() {
-  $('table.screen-keys td:not(.empty-cell)').addClass('highlight');
-
-  setTimeout(function() {
-    $('table.screen-keys td').removeClass('highlight');
-  }, 300);
-}
-
-$('img').mousedown(highlightControls);
-$('table.screen-keys td').mousedown(highlightControls);
